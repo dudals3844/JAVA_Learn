67379157093e0d473b2bc91d9ee6e156abf37153
Young Min <dudals3844@gmail.com>
2019-09-22 13:26:59 +0000

    learn method multiple returns

--- src/project/Main.java
+++ src/project/Main.java
@@ -3,31 +3,24 @@ package project;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
 public class Main {
 
     /**
      * @param args
      */
-    public static String numbering(int init,int limit){ //static은 정적 자료이고 String은 리턴을 String으로 한다는 의미
-        int i = init;
-        
-        String output = "";//숫자들을 아웃풋에 담는다. 
-        while(i < limit){
-            //System.out.println(i);
-            output += i;
-            i++;
-        }
-        
-        return output;//public static String 이므로 String을 리턴한다.
+    public static String[] getMembers(){
+        String[] members = {"choi", "young", "min"};
+        return members;
     }
     
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        //numbering();
-        String result = numbering(1,5);
-        System.out.println(result);
-        
+        String[] members = getMembers();
+        for(int i =0 ; i < members.length; i++){
+            System.out.print(members[i]);
+        }
         
         
     }
