559836c94fd6c02c56658cade6253ba2aadaedb2
Young Min <dudals3844@gmail.com>
2019-09-22 13:21:47 +0000

    learn method return

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,19 +8,25 @@ public class Main {
     /**
      * @param args
      */
-    public static void numbering(int limit){
-        int i = 0;
+    public static String numbering(int init,int limit){ //static은 정적 자료이고 String은 리턴을 String으로 한다는 의미
+        int i = init;
+        
+        String output = "";//숫자들을 아웃풋에 담는다. 
         while(i < limit){
-            System.out.println(i);
+            //System.out.println(i);
+            output += i;
             i++;
         }
+        
+        return output;//public static String 이므로 String을 리턴한다.
     }
     
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         //numbering();
-        numbering(5);
+        String result = numbering(1,5);
+        System.out.println(result);
         
         
         
