b1e6882444a88a8b19be46fdd9697358a9321591
Young Min <dudals3844@gmail.com>
2019-09-22 13:08:44 +0000

    learn method

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,19 +8,18 @@ public class Main {
     /**
      * @param args
      */
+    public static void numbering(){
+        int i = 0;
+        while(i < 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+    
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        HashMap<String, String> map = new HashMap<String , String>();
-        map.put("people", "human");
-        map.put("baseball","야구");
-        
-        
-        System.out.println(map.get("people"));
-        System.out.println(map.get("baseball"));
-        
-        System.out.println(map.containsKey("people"));
-        System.out.println(map.remove("people"));
-        System.out.println(map.get("people"));
+        numbering();
         
         
     }
