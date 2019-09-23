5ff1a92577da67b15b32b7b0e19bd289106e214c
Young Min <dudals3844@gmail.com>
2019-09-23 13:19:46 +0000

    learn variable effective range

--- src/project/Main.java
+++ src/project/Main.java
@@ -9,19 +9,21 @@ public class Main{
     /**
      * @param args
      */
-    static int i;
+    static int i = 5;
     
     static void a(){
-        i = 0;
+        int i = 0;//전역변수보다 지역변수가 우선이다. ㅑi = 0은 전역변수  int i = 0은 지역변수이다
+        b();
+    }
+    
+    static void b(){
+        System.out.println(i);
     }
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         
-        for(i = 0; i < 5; i++){
-            a();
-            System.out.println(i);
-        }
+        a();
        
 
     }
