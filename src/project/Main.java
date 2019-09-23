59352f84ff2d59dd5b97d2af60fedb2ac25b8d8d
Young Min <dudals3844@gmail.com>
2019-09-23 13:15:19 +0000

    learn global variable

--- src/project/Main.java
+++ src/project/Main.java
@@ -9,16 +9,16 @@ public class Main{
     /**
      * @param args
      */
-    //static int i;
+    static int i;
     
     static void a(){
-        int i = 0;
+        i = 0;
     }
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         
-        for(int i = 0; i < 5; i++){
+        for(i = 0; i < 5; i++){
             a();
             System.out.println(i);
         }
