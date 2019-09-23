6f4f75ce7590642ec36037e3e73e6a36f7fad934
Young Min <dudals3844@gmail.com>
2019-09-23 13:23:24 +0000

    learn instance effective range

--- src/project/Main.java
+++ src/project/Main.java
@@ -4,27 +4,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
+class C {
+    int v = 10;
+    
+    
+    void m(){
+        int v  = 20;
+        //v = this.v;
+        System.out.println(v);
+    }
+}
+
+
 
 public class Main{
     /**
      * @param args
      */
-    static int i = 5;
     
-    static void a(){
-        int i = 0;//전역변수보다 지역변수가 우선이다. ㅑi = 0은 전역변수  int i = 0은 지역변수이다
-        b();
-    }
-    
-    static void b(){
-        System.out.println(i);
-    }
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         
-        a();
-       
-
+        C c1 = new C();
+        c1.m();
+        
     }
 }
