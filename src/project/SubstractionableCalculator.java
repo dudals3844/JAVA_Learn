b7e6869c967b175a96103c070037de62fe7f574f
Young Min <dudals3844@gmail.com>
2019-09-23 14:28:53 +0000

    learn error overriding

--- src/project/SubstractionableCalculator.java
+++ src/project/SubstractionableCalculator.java
@@ -7,6 +7,13 @@ class SubstractionableCalculator extends Calculator {
         System.out.println("실행결과는 " + (this.left + this.right) + "입니다.");    
     }
     
+    //overriding
+    public int avg() {
+        return (this.left + this.right)/2;
+    }//error syntax
+    // 메소드의 이름
+    // 메소드 매개변수의 숫자와 데이터 타입 그리고 순서
+    // 메소드의 리턴 타입
     
     public void substract(){
         System.out.println(this.left - this.right);
