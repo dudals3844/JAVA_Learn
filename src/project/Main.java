e3f245211b65d85469c69073dbf7a2df6aeea527
Young Min <dudals3844@gmail.com>
2019-09-23 14:03:04 +0000

    learn error constructor

--- src/project/Main.java
+++ src/project/Main.java
@@ -9,11 +9,11 @@ public class Main{
     /**
      * @param args
      */
-    
+    public Main (int param){}
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         Main m  = new Main();
-        //자동으로 생성자를 만들어준다. 
+        //public Main (int param){}가 만들어져 자동으로 생성자가 만들어지지 않는다
     }
 }
