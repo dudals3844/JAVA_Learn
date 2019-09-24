2a0b231825203387932f5f9b3bfbe1fb98ee4317
Young Min <dudals3844@gmail.com>
2019-09-24 14:06:12 +0000

    learn final method

--- src/project/A.java
+++ src/project/A.java
@@ -4,7 +4,7 @@ abstract class A {
     
     public abstract int b();
     
-    public void d(){
+    final void d(){
         //추상클래스 안에는 추상 메소드가 아닌 메소드도 들어갛 수 있다
         System.out.println("world");
     }
--- src/project/B.java
+++ src/project/B.java
@@ -2,6 +2,6 @@ package project;
 
 class B extends A {
     public int b(){
-        return 1;
+        //final로 되어있는 b()를 상속하려해서 오류가 생김
     }
 }
