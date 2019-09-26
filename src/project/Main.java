06e681641580c9a907f8a91c8b495d58acb48fb2
Young Min <dudals3844@gmail.com>
2019-09-26 13:16:43 +0000

    learn parameter enum constructor

--- src/project/Main.java
+++ src/project/Main.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 enum Fruit {
-    APPLE, PEACH, BANANA;
-    Fruit(){
+    APPLE("red"), PEACH("pink"), BANANA("yellow");
+    public String color;
+    Fruit(String color){//APPLE("red")가 생성자를 호출한다 호출된 생성자는 Fruit(String color)이다
         System.out.println("Call Constructor "+this);
+        this.color = color;
     }
     
     // public Fruit(){//enum은 private만 지원해서 오류가 난다.
@@ -28,13 +30,13 @@ public class Main{
         Fruit type = Fruit.APPLE;
         switch(type){
             case APPLE:
-                System.out.println(57+" kcal");
+                System.out.println(57+" kcal: "+Fruit.APPLE.color);
                 break;
             case PEACH:
-                System.out.println(34+" kcal");
+                System.out.println(34+" kcal: "+Fruit.PEACH.color);
                 break;
             case BANANA:
-                System.out.println(93+" kcal");
+                System.out.println(93+" kcal: "+Fruit.BANANA.color);
                 break;
         }
     }
